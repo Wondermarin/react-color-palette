fda0555aeb7dd3354eb506880cb17dd940028029
Wondermarin <thekarma4@gmail.com>
2020-10-11 11:59:21 +0300

    v2.0.2

--- src/components/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ColorPicker, { useColorObject } from "./ColorPicker";
-
-export const App = (): JSX.Element => {
-  const [color, setColor] = useColorObject("hex", "#121212");
-
-  return <ColorPicker width={400} color={color} onChange={setColor} />;
-};
