d316761f3eda12e832aca6e5c911aeee5240c22b
Alexander <33459274+Wondermarin@users.noreply.github.com>
2021-02-20 10:51:23 +0300

    fix: new interfaces

--- src/components/Fields/Fields.types.ts
+++ src/components/Fields/Fields.types.ts
@@ -1,9 +1,22 @@
 import { Color } from "../../utils/toColor";
 
-export interface FieldsProps {
+export interface UpperFloorProps {
+  readonly color: Color;
+  readonly hideHEX: boolean;
+  readonly onChange: (color: Color) => void;
+}
+
+export interface LowerFloorProps {
   readonly color: Color;
   readonly hideRGB: boolean;
+  readonly hideHSB: boolean;
+  readonly onChange: (color: Color) => void;
+}
+
+export interface FieldsProps {
+  readonly color: Color;
   readonly hideHEX: boolean;
+  readonly hideRGB: boolean;
   readonly hideHSB: boolean;
   readonly onChange: (color: Color) => void;
 }
