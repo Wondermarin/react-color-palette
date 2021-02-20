00894e465335e0782601b59b2e4fb270336b4e36
Alexander <33459274+Wondermarin@users.noreply.github.com>
2021-02-20 10:46:44 +0300

    fix: argument destructuring

--- src/components/ColorPicker/ColorPicker.component.tsx
+++ src/components/ColorPicker/ColorPicker.component.tsx
@@ -4,27 +4,23 @@ import { Hue } from "../Hue";
 import { Fields } from "../Fields";
 import "./styles.css";
 
-const ColorPicker = (props: ColorPickerProps): JSX.Element => {
-  const {
-    width,
-    height = width,
-    color,
-    onChange,
-    dark = false,
-    hideHEX = false,
-    hideHSB = false,
-    hideRGB = false,
-  } = props;
-
-  return (
-    <div className={`rcp ${dark ? "rcp-dark" : "rcp-light"}`}>
-      <Saturation width={width} height={height} color={color} onChange={onChange} />
-      <div className="rcp-body" style={{ width: width - 32 }}>
-        <Hue width={width - 32} color={color} onChange={onChange} />
-        <Fields color={color} onChange={onChange} hideHEX={hideHEX} hideHSB={hideHSB} hideRGB={hideRGB} />
-      </div>
+const ColorPicker = ({
+  width,
+  height = width,
+  color,
+  onChange,
+  hideHEX = false,
+  hideRGB = false,
+  hideHSB = false,
+  dark = false,
+}: ColorPickerProps): JSX.Element => (
+  <div className={`rcp ${dark ? "rcp-dark" : "rcp-light"}`}>
+    <Saturation width={width} height={height} color={color} onChange={onChange} />
+    <div className="rcp-body">
+      <Hue width={width - 32} color={color} onChange={onChange} />
+      <Fields color={color} hideHEX={hideHEX} hideRGB={hideRGB} hideHSB={hideHSB} onChange={onChange} />
     </div>
-  );
-};
+  </div>
+);
 
 export default ColorPicker;
