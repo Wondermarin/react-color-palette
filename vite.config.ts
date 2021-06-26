daa401e925da55415db30fa364ebe3a4aea519ae
Wondermarin <33459274+Wondermarin@users.noreply.github.com>
2021-06-26 08:00:32 +0300

    chore: `Parcel` -> `Vite`

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+export default defineConfig({
+  root: "demo",
+  base: "./",
+  plugins: [reactRefresh()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    assetsInlineLimit: 0,
+  },
+});
