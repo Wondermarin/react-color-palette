a9660fbb91e0f6bf5e56a113672ce541d80c79b5
unknown <online7890@gmail.com>
2021-09-09 16:24:04 +0300

    #20 added support for touch devices & improved performance

--- src/components/Interactive.component.tsx
+++ src/components/Interactive.component.tsx
@@ -1,42 +1,112 @@
-import React, { useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { InteractiveProps } from "../interfaces/Interactive.interface";
 import { clamp } from "../utils/clamp.util";
+
+const getPointerPositionPoint = (
+  event: MouseEvent | TouchEvent
+): {
+  x: number;
+  y: number;
+} => {
+  if (event instanceof TouchEvent) {
+    return {
+      x: event.touches[0].clientX,
+      y: event.touches[0].clientY,
+    };
+  } else {
+    return {
+      x: event.clientX,
+      y: event.clientY,
+    };
+  }
+};
 
 export const Interactive = ({ className, style, onChange, children }: InteractiveProps): JSX.Element => {
   const divRef = useRef<HTMLDivElement>(null);
 
-  const move = (e: React.MouseEvent | MouseEvent): void => {
-    if (divRef.current) {
-      const { current: div } = divRef;
-      const { width, height, left, top } = div.getBoundingClientRect();
+  console.log(1);
 
-      const x = clamp(e.clientX - left, width, 0);
-      const y = clamp(e.clientY - top, height, 0);
+  const move = useCallback(
+    (clientX: number, clientY: number): void => {
+      if (divRef.current) {
+        const { current: div } = divRef;
+        const { width, height, left, top } = div.getBoundingClientRect();
 
-      onChange(x, y);
+        const x = clamp(clientX - left, width, 0);
+        const y = clamp(clientY - top, height, 0);
+
+        onChange(x, y);
+      }
+    },
+    [onChange]
+  );
+
+  const onPointerMove = useCallback(
+    (event: MouseEvent | TouchEvent): void => {
+      const point = getPointerPositionPoint(event);
+
+      move(point.x, point.y);
+    },
+    [move]
+  );
+
+  const onPointerUp = useCallback((): void => {
+    document.removeEventListener("mousemove", onPointerMove);
+    document.removeEventListener("mouseup", onPointerUp);
+    document.removeEventListener("touchmove", onPointerMove);
+    document.removeEventListener("touchend", onPointerUp);
+  }, [onPointerMove]);
+
+  const onPointerDown = useCallback(
+    (event: MouseEvent | TouchEvent): void => {
+      if (event instanceof MouseEvent && event.button !== 0) {
+        return;
+      } else if (event.cancelable) {
+        event.preventDefault();
+      }
+
+      const point = getPointerPositionPoint(event);
+
+      move(point.x, point.y);
+
+      document.addEventListener("mousemove", onPointerMove, {
+        passive: false,
+      });
+      document.addEventListener("mouseup", onPointerUp, {
+        passive: false,
+      });
+      document.addEventListener("touchmove", onPointerMove, {
+        passive: false,
+      });
+      document.addEventListener("touchend", onPointerUp, {
+        passive: false,
+      });
+    },
+    [move, onPointerMove, onPointerUp]
+  );
+
+  useEffect(() => {
+    const div = divRef.current;
+
+    if (div) {
+      div.addEventListener("mousedown", onPointerDown, {
+        passive: false,
+      });
+      div.addEventListener("touchstart", onPointerDown, {
+        passive: false,
+      });
     }
-  };
 
-  const onMouseDown = (e: React.MouseEvent): void => {
-    if (e.button !== 0) return;
-
-    move(e);
-
-    const onMouseMove = (e: MouseEvent): void => {
-      move(e);
+    return (): void => {
+      if (div) {
+        div.removeEventListener("mousedown", onPointerDown);
+        div.removeEventListener("touchstart", onPointerDown);
+      }
     };
-
-    const onMouseUp = (): void => {
-      document.removeEventListener("mousemove", onMouseMove, false);
-      document.removeEventListener("mouseup", onMouseUp, false);
-    };
-
-    document.addEventListener("mousemove", onMouseMove, false);
-    document.addEventListener("mouseup", onMouseUp, false);
-  };
+  }, [onPointerDown]);
 
   return (
-    <div ref={divRef} className={className} style={style} onMouseDown={onMouseDown}>
+    <div ref={divRef} className={className} style={style}>
       {children}
     </div>
   );
